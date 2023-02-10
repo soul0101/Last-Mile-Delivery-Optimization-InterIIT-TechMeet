652a859b399ed80212cd8f2f45db24c34c956554
= <=>
2023-02-10 17:12:17 +0530

    added streamlit

--- Implementation/main.py
+++ Implementation/main.py
@@ -11,8 +11,8 @@ from datetime import timedelta
 if __name__ == '__main__':
     
     # depot, orders, vehicles = helper.generate_random_problem(num_orders=50)
-    mock_dispatch_filename = os.path.dirname(__file__) + r'\mock\dispatch_testing.xlsx'
-    mock_pickup_filename = os.path.dirname(__file__) + r'\mock\pickups_testing.xlsx'
+    mock_dispatch_filename = os.path.join(os.path.dirname(__file__), 'mock/dispatch_testing.xlsx')
+    mock_pickup_filename = os.path.join(os.path.dirname(__file__),'mock/pickups_testing.xlsx')
     depot, orders, vehicles = helper.generate_problem_from_file(mock_dispatch_filename, mock_pickup_filename)
     vrp_instance = VRP(depot, orders, vehicles)
 
