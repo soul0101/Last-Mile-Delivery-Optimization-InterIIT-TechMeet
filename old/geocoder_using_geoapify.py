a5f57f7806eedadc14d2147daac8f99eddbb4ba9
soul0101 <53980340+soul0101@users.noreply.github.com>
2023-03-02 15:26:06 +0530

    restructure

--- old/geocoder_using_geoapify.py
+++
@@ -1,8 +0,0 @@
-import requests, urllib
-
-def geocode(address): #returns [x, y]
-    url="https://api.geoapify.com/v1/geocode/search?text="+ urllib.parse.quote(address)+"&apiKey=dddb306415724bfe99d6882228f04fef"
-    resp = requests.get(url).json()
-    return resp['features'][0]['geometry']['coordinates']
-
-print(geocode('6 Shakambari Nagar, 1st stage, JP Nagar, Bangalore'))
